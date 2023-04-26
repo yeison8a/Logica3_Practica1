292b5f0022fdc129128135ffd02a6cfc8003be8a
yeison8a <yeison.ochoa@udea.edu.co>
2023-04-26 17:08:34 -0500

    proyecto

---
+++ src/Persona.java
@@ -0,0 +1,37 @@
+public class Persona {
+    private String nombre;
+    private String genero;
+    private int edad;
+
+    public Persona(String nombre, String genero, int edad){
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
+}
